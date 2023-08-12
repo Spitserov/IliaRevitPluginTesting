d075e7f20c515bf56b1e2d4bf186617310f37781
ilia spitserov <iliaspitserov@gmail.com>
2023-08-13 00:23:41 +0300

    IExternalApplication toolbar added
    added a test toolbar with 2 buttons

---
+++ IliasRevitPluginTest_01/Commands/ShowDocInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace IliasRevitPluginTest_01
+{
+    [TransactionAttribute(TransactionMode.ReadOnly)]
+    public class IliasTest_ShowDocInfo : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            TaskDialog.Show("task dialog shows:", "THIS");
+
+            return Result.Succeeded;          
+        }
+
+    }
+}
---
+++ IliasRevitPluginTest_01/IliasRevitToolbarTest_01.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using System.Reflection;
+using Autodesk.Revit.Attributes;
+
+namespace IliasRevitPluginTest_01
+{
+    [TransactionAttribute(TransactionMode.ReadOnly)]
+    class ToolbarExternalApplication : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            //Create Ribbon Tab
+            application.CreateRibbonTab("Ilias Commands Tab");
+
+            //Create Pushbutton
+            string assemblyPath = Assembly.GetExecutingAssembly().Location;
+            PushButtonData button01 = new PushButtonData("button01", "MyButton_01", assemblyPath, "IliasRevitPluginTest_01.IliasTest_GetElementId");
+            PushButtonData button02 = new PushButtonData("button02", "MyButton_02", assemblyPath, "IliasRevitPluginTest_01.IliasTest_ShowDocInfo");
+
+
+            //Create Ribbon Panel
+            RibbonPanel commandsPanel = application.CreateRibbonPanel("Ilias Commands Tab", "Ilias Commands Panel");
+            commandsPanel.AddItem(button01);
+            commandsPanel.AddItem(button02);
+
+
+            return Result.Succeeded;
+        }
+    }
+}
