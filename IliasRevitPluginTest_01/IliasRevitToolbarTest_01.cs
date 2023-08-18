430baad070a8c3a4e44e16c0331b1f80a3e7b847
ilia spitserov <iliaspitserov@gmail.com>
2023-08-18 14:27:58 +0300

    gitignore settings work! test commit

--- IliasRevitPluginTest_01/IliasRevitToolbarTest_01.cs
+++ IliasRevitPluginTest_01/IliasRevitToolbarTest_01.cs
@@ -8,6 +8,8 @@ using System.Reflection;
 using Autodesk.Revit.Attributes;
 using System.Windows.Media.Imaging;
 
+//TestComment
+
 namespace IliasRevitPluginTest_01
 {
     [TransactionAttribute(TransactionMode.ReadOnly)]
--- IliasRevitPluginTest_01/UI/WPF_Test_01.xaml.cs
+++ IliasRevitPluginTest_01/UI/WPF_Test_01.xaml.cs
@@ -13,8 +13,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-//AnotherTest
-
 namespace IliasRevitPluginTest_01.UI
 {
     /// <summary>
